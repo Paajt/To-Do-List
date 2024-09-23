64bd817fd46d3a42882fb9ed03b33943f4d8fa35
Patrik <pat_p3@hotmail.com>
2024-09-23 07:39:40 +0200

    added completed tasks counter

--- script.js
+++ script.js
@@ -4,13 +4,20 @@
 const inputTodo = document.querySelector('#inputTodo');
 const addButton = document.querySelector('#addButton');
 const tasksLeft = document.querySelector('#tasksLeft');
+const tasksDone = document.querySelector('#tasksDone');
 const todoList = document.querySelector('#todoList');
 const todoArray = [];
+let tasksCompleted = 0;
 let taskCounter = 0;
 
 // Function to update the task counter
 function updateTaskCounter() {
     tasksLeft.innerText = `Tasks left: ${taskCounter}`;
+}
+
+// Function to update the tasks completed counter
+function updateTasksCompleted() {
+    tasksDone.innerText = `Tasks completed: ${tasksCompleted}`;
 }
 
 // EventListener for clicking the add button
@@ -43,7 +50,7 @@ addButton.addEventListener(
         itemLabel.innerText = text;
         listItem.appendChild(itemLabel);
 
-        // Add new delete button element in html with trash can icon
+        // Add delete button element in html with trash can icon
         let deleteButton = document.createElement('button');
         deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
         deleteButton.type = 'button';
@@ -58,15 +65,18 @@ addButton.addEventListener(
                 //Add and remove class for completed task
                 itemLabel.classList.toggle('completed');
 
-                // Update counter: when task is clicked on/completed and updates if uncompleted again
+                // Update both counters: when task is clicked on/completed and updates if uncompleted again
                 if (itemLabel.classList.contains('completed')) {
                     taskCounter--;
+                    tasksCompleted++;
                 }
                 else {
                     taskCounter++;
+                    tasksCompleted--;
                 }
 
                 updateTaskCounter();
+                updateTasksCompleted();
 
             }
         );
@@ -83,9 +93,10 @@ addButton.addEventListener(
             // Remove the list item from DOM
             todoList.removeChild(listItem);
 
-            // Decrease task count only if the task was clicked on/completed
+            // Decrease task count only if the task was clicked on/completed & increase completed tasks
             if (!itemLabel.classList.contains('completed')) {
                 taskCounter--;
+                tasksCompleted++;
             }
 
             // Update task counter
